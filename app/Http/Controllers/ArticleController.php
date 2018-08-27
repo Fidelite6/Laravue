f69505a2c942183830d603a8a5dea5edb5a53ad8
yurii <fidelite62@gmail.com>
2018-08-27 22:14:34 +0300

    add crud methods, defaultStringLength

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,84 +2,65 @@
 
 namespace App\Http\Controllers;
 
-use App\Article;
 use Illuminate\Http\Request;
+use App\Article;
+use App\Http\Resources\Article as ArticleResource;
 
 class ArticleController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        //
-    }
+        $articles = Article::paginate(15);
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return ArticleResource::collection($articles);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request $request
+     * @return ArticleResource
      */
     public function store(Request $request)
     {
-        //
+        // If method is PUT - find by id and save article, otherwise create new Article
+        $article = $request->isMethod('put') ? Article::findOrFail($request->article_id) : new Article();
+
+        $article->id = $request->input('article_id');
+        $article->title = $request->input('title');
+        $article->body = $request->input('body');
+
+        if ($article->save())
+            return new ArticleResource($article);
+
     }
 
     /**
      * Display the specified resource.
-     *
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
+     * @param Article $article
+     * @return ArticleResource
      */
     public function show(Article $article)
     {
-        //
+        return new ArticleResource($article);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Article $article)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Article $article)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
+     * @param  \App\Article $article
+     * @return ArticleResource
      */
     public function destroy(Article $article)
     {
-        //
+        if ($article->delete()) {
+            return new ArticleResource($article);
+        }
     }
 }
